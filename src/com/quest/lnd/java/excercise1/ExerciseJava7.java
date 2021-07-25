73d6e090bf6638c28dcfab149f7e585323bbfd34
Vishnoo Rath <vishnoo.rath@gmail.com>
2021-07-25 21:58:20 +0530

    Added Excercise for Java 7

---
+++ src/com/quest/lnd/java/excercise1/ExerciseJava7.java
@@ -0,0 +1,25 @@
+package com.quest.lnd.java.excercise1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExerciseJava7 {
+
+    public static void main(String[] args)
+    {
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Carrol", 42),
+                new Person("Thomas", "Carlyle", 53),
+                new Person("Charlotte", "Bronte", 45),
+                new Person("Mathew", "Arnold", 39)
+        );
+    }
+
+    //1. Sort list by Last Name;
+
+    //2. Create a method that prints all elements in the list
+
+    //3. Create a method that prints all elements with last name as 'C'
+
+}
